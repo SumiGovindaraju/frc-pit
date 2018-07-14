330991c9f1fa69fc6aa1273ecc6a9986dd437fa1
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-07-13 18:36:17 -0700

    fix one more link

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,7 @@ export default class Header extends Component {
     return (
       <header>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="../">FRC Pit</a>
+          <a className="navbar-brand" href="../frc-pit">FRC Pit</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
