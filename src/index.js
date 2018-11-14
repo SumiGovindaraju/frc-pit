6997b90ab35ecc36fda1d19c57163e7e55543fdf
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-11-13 21:10:44 -0800

    Try caching css using react sw

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/bootstrap.min.css';
+import './css/fontawesome-all.min.css';
+import './css/style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import initFirebase from './initFirebase';
