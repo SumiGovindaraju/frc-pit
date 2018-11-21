b2caac8b632ec0c1bacdd960630e212eaa37b5a6
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-11-21 15:59:54 -0800

    Revert routing changes

--- src/routes.js
+++ src/routes.js
@@ -9,37 +9,37 @@ import Statistics from './pages/Statistics';
 
 export default [
   {
-    path: process.env.PUBLIC_URL + '/',
+    path: '/',
     component: Home,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/auth',
+    path: '/auth',
     component: Auth,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/tools_list',
+    path: '/tools_list',
     component: ToolsList,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/check_out_tool',
+    path: '/check_out_tool',
     component: CreateNewTool,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/signed_in',
+    path: '/signed_in',
     component: SignedIn,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/sign_out',
+    path: '/sign_out',
     component: SignOut,
     exact: true
   },
   {
-    path: process.env.PUBLIC_URL + '/stats',
+    path: '/stats',
     component: Statistics,
     exact: true
   },
