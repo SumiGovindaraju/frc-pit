297e2fc3f277ac74c9c4b95cab77fd71da12a73a
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2018-12-14 17:53:35 -0800

    Add sorting for statistics

--- src/components/StatisticsTable.js
+++ src/components/StatisticsTable.js
@@ -1,6 +1,55 @@
 import React, { Component } from 'react';
 
 export default class StatisticsTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sortStatistics = this.sortStatistics.bind(this);
+  }
+
+  // from w3 schools
+  sortStatistics(n) {
+    if (document.readyState !== "complete") {
+      return;
+    }
+
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("statistics").getElementsByTagName("table")[0];
+    switching = true;
+    dir = "asc";
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("td")[n];
+        y = rows[i + 1].getElementsByTagName("td")[n];
+        if (dir === "asc") {
+          if (parseFloat(x.innerHTML) > parseFloat(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir === "desc") {
+          if (parseFloat(x.innerHTML) < parseFloat(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        switchcount++;
+      } else {
+        if (switchcount === 0 && dir === "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
   render() {
     return (
       <div className="statistics" id="statistics">
@@ -8,16 +57,16 @@ export default class StatisticsTable extends Component {
         <table className="table table-striped table-bordered">
           <thead>
             <tr>
-              <th>Rank</th>
-              <th>Team</th>
-              <th>Ranking Score</th>
-              <th>Park/Climb Points</th>
-              <th>Auto</th>
-              <th>Ownership</th>
-              <th>Vault</th>
+              <th onClick={() => this.sortStatistics(0)}>Rank <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(1)}>Team <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(2)}>Ranking Score <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(3)}>Park/Climb Points <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(4)}>Auto <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(5)}>Ownership <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(6)}>Vault <i className="fas fa-sort"></i></th>
               <th>Record (W-L-T)</th>
-              <th>Played</th>
-              <th>Total RP</th>
+              <th onClick={() => this.sortStatistics(8)}>Played <i className="fas fa-sort"></i></th>
+              <th onClick={() => this.sortStatistics(9)}>Total RP <i className="fas fa-sort"></i></th>
               <th>View Team Info</th>
             </tr>
           </thead>
