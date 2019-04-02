7315c4fa24b7d6989659d48441d91267e9d678d4
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2019-04-02 16:56:31 -0700

    Make countdown reflect bumper color

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -5,7 +5,7 @@ export default class Countdown extends Component {
     return (
       <div className="countdown">
         <h1 className="no-countdown" style={{ display: "none" }}>No Match Coming Up</h1>
-        <h1 className="countdown-timer-tag text-danger" style={{ textAlign: "center", fontSize: "4.5em" }}>&nbsp;</h1>
+        <h1 className="countdown-timer-tag" style={{ textAlign: "center", fontSize: "5.25em" }}>&nbsp;</h1>
         <h2 style={{ textAlign: "center" }}>Until </h2>
       </div>
     );
