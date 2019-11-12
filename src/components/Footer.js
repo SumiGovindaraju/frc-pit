78131902c8d7be184ed2cfa91346f9bffe6a1eb8
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2019-11-11 19:46:02 -0800

    fix some footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default class Footer extends Component {
   render() {
     return (
         <div className="footer">
-            <p>Copyright (c) 2018-{(new Date()).getFullYear()} <a target="_blank" href="https://sumigovindaraju.github.io/" rel="noopener noreferrer">SumiGovindaraju</a>. Powered by <a target="_blank" href="https://www.thebluealliance.com/" rel="noopener noreferrer">The Blue Alliance</a> and <a target="_blank" href="https://firebase.google.com/" rel="noopener noreferrer">Firebase</a>.</p>
+          <p>Copyright (c) 2018-{(new Date()).getFullYear()} <a target="_blank" href="https://sumigovindaraju.github.io/" rel="noopener noreferrer">SumiGovindaraju</a>. Powered by <a target="_blank" href="https://reactjs.org/" rel="noopener noreferrer">React</a>, <a target="_blank" href="https://www.thebluealliance.com/" rel="noopener noreferrer">The Blue Alliance</a>, <a target="_blank" href="https://firebase.google.com/" rel="noopener noreferrer">Firebase</a>, and <a target="_blank" href="https://pages.github.com/" rel="noopener noreferrer">GitHub Pages</a>.</p>
         </div>
     );
   }
