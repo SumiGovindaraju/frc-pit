b9678a8f7611564df9bdc178d9f7b2377c0e1e69
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2019-11-15 18:28:51 -0800

    printenv

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import initFirebase from './util/initFirebase';
 
+console.log(process.env);
 initFirebase();
 
 ReactDOM.render(<App />, document.getElementById('root'));
